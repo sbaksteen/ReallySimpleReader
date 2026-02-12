f743e1af989063e505c59eedbe72aa79782055ec
sbaksteen <mail@sbaksteen.nl>
2026-02-12 10:31:30 +0100

    Dummy React.js frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={
+          () => axios.get("http://localhost:8000/api/hello")
+          .then((response) => {
+            if (response.status !== 200) {
+              setMessage(`HTTP status ${response.status}`);
+              return;
+            }
+            setMessage(response.data.message);
+          })
+        }>message is {message}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
